54044d702bc204c55de8df8b2b6d5b2013d17f72
Kuba <jkb.kam@gmail.com>
2018-05-09 00:48:21 +0200

    have finished on commit nr 3eb1fa5 (bottom of the page)

--- src/TaskView.js
+++ src/TaskView.js
@@ -24,14 +24,19 @@ class TaskView extends Component {
         event.preventDefault()
         console.log(this.state.taskName, this.state.taskDescription)
 
+        this.setState({
+            taskName: '',
+            taskDescription: ''
+        })
 
         this.setState({
             tasks: this.state.tasks.concat({
-                id: this.state.tasks.length + 1,
+                id: Math.max(...this.state.tasks.map(task =>task.id))+1,
                 name: this.state.taskName,
                 description: this.state.taskDescription
             })
         })
+
     }
         handleChange = event => {
             this.setState({
